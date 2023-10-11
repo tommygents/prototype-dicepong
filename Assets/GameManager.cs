831215cecfbf4e4e37bb15e98f0488c63cdc822d
tommygents <tmaranges+gh@gmail.com>
2023-10-11 16:55:11 -0400

    saved and pushed up

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -25,7 +25,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Score())
+        {
+            theBall.ResetBall();
+
+        }
     }
 
    
